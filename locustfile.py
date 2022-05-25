09144d7002476e8c5bd07529f6d9551450272fe0
vic-shihang-li <40274755+vic-shihang-li@users.noreply.github.com>
2022-05-25 10:08:07 -0400

    auto-format

--- locustfile.py
+++ locustfile.py
@@ -1,5 +1,6 @@
 from locust import HttpUser, task
 
+
 class TrainTicketUser(HttpUser):
     # Auth token used for certain API routes that require authentication.
     #
@@ -9,27 +10,32 @@ class TrainTicketUser(HttpUser):
 
     @task
     def hello_world(self):
-        payload = {\
-                "startingPlace":"Shang Hai",\
-                "endPlace":"Su Zhou",\
-                "departureTime":"2022-05-31"}
-        self.client.get("/api/v1/travelservice/trips/left",params = payload)
-    
+        payload = {
+            "startingPlace": "Shang Hai",
+            "endPlace": "Su Zhou",
+            "departureTime": "2022-05-31",
+        }
+        self.client.get("/api/v1/travelservice/trips/left", params=payload)
+
     @task
     def assurance(self):
-        self.client.get("/api/v1/assuranceservice/assurances/types",
-                headers=self.auth_header())
+        self.client.get(
+            "/api/v1/assuranceservice/assurances/types", headers=self.auth_header()
+        )
 
     @task
     def foodservice(self):
         self.client.get(
-                "/api/v1/foodservice/foods/2022-05-31/Shang%20Hai/Su%20Zhou/D1345")
+            "/api/v1/foodservice/foods/2022-05-31/Shang%20Hai/Su%20Zhou/D1345"
+        )
 
     @task
     def contacts(self):
         account_id = "4d2a46c7-71cb-4cf1-b5bb-b68406d9da6f"
-        self.client.get(f"/api/v1/contactservice/contacts/account/{account_id}",
-                headers=self.auth_header())
+        self.client.get(
+            f"/api/v1/contactservice/contacts/account/{account_id}",
+            headers=self.auth_header(),
+        )
 
     def auth_header(self):
         return {"Authorization": f"Bearer {TrainTicketUser.AUTH_TOKEN}"}
